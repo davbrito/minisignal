c6c70b827789d35a7b152b0bd68bbe43e8bf5fd0
David Brito <39559632+davbrito@users.noreply.github.com>
2025-06-22 15:15:56 -0400

    chore: update deps

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,18 +1,7 @@
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-import matchers from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom/vitest";
+
 import { cleanup } from "@testing-library/react";
-import { afterEach, expect } from "vitest";
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace Vi {
-    interface JestAssertion<T>
-      extends jest.Matchers<void, T>,
-        TestingLibraryMatchers<T, void> {}
-  }
-}
-
-expect.extend(matchers);
+import { afterEach } from "vitest";
 
 afterEach(() => {
   cleanup();
